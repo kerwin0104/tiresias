783cef6cf0bbfcf46566899ed9246e592022d5aa
jielang <jielang19940627@163.com>
2017-09-28 18:51:40 +0800

    fix static root

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var config = require('./config')
 var express = require('express')
 var exphbs  = require('express-handlebars');
 var kernel = require('./kernel')
+var path = require('path')
 var app = express()
-app.use(express.static('static'))
+app.use(express.static(path.join(config.rootDir, 'static')))
 app.use(kernel.routeHelper(config))
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
