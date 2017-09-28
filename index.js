1deed760af36a97cdd56a80dfb41979b7d00105b
jielang <jielang19940627@163.com>
2017-09-28 18:40:22 +0800

    add static fileDir

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var express = require('express')
 var exphbs  = require('express-handlebars');
 var kernel = require('./kernel')
 var app = express()
-
+app.use(express.static('static'))
 app.use(kernel.routeHelper(config))
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
-var port = 8080
+var port = 3000
 console.log('server listen at port:' + port)
 app.listen(port)
 
